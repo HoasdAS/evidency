7fb78fa278b8d15b4a483d5402a414aa70698a49
Alumno IDS <alumnoids@iMac-de-Sistemas.local>
2018-04-20 21:36:36 -0500

    evidency

---
+++ Podfile
@@ -0,0 +1,20 @@
+platform :ios, '9.0'
+
+
+source 'https://github.com/CocoaPods/Specs.git'
+target 'evidencia' do
+  use_frameworks!
+pod 'GoogleMaps'
+pod 'GooglePlaces'
+
+  target 'evidenciaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'evidenciaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  evidencia
+//
+//  Created by Alumno IDS on 20/04/18.
+//  Copyright © 2018 evidenciaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
